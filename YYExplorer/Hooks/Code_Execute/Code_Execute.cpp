cb0991260120d79d77ab40c4953d623e51f7af60
Archie <pin979xp@gmail.com>
2021-10-12 15:22:39 +0200

    Remove YYExplorer
    Not needed now that the console is a thing

--- YYExplorer/Hooks/Code_Execute/Code_Execute.cpp
+++
@@ -1,6 +0,0 @@
-#include "../Hooks.hpp"
-
-void Hooks::Code_Execute(YYTKCodeEvent* _evt, CInstance* _pSelf, CInstance* _pOther, CCode* _pCode, RValue* _Args)
-{
-
-}
--- YYExplorer/Menu/Menu.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace Menu
-{
-	void Draw();
-}
