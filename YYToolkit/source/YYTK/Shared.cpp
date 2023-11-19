b163fb220bbe38db37e5dec8c1b0bf69fd6b8288
Archie_UwU <pin979xp@gmail.com>
2023-11-19 13:31:09 +0100

    Direct RValue string access*
    * provided you have the interface.
    
    - Adds RValue constructors for booleans
    - Adds AsBool, AsReal, and AsString methods - use the runner interface for the other types.

--- YYToolkit/source/YYTK/Shared.cpp
+++ YYToolkit/source/YYTK/Shared.cpp
@@ -1,4 +1,5 @@
 #include "Shared.hpp"
+#include <cassert>
 using namespace Aurie;
 
 YYTK::RValue::RValue()
@@ -6,6 +7,15 @@ YYTK::RValue::RValue()
 	this->m_Real = 0;
 	this->m_Flags = 0;
 	this->m_Kind = VALUE_UNSET;
+}
+
+YYTK::RValue::RValue(
+	IN bool Value
+)
+{
+	this->m_Real = static_cast<double>(Value);
+	this->m_Flags = 0;
+	this->m_Kind = VALUE_BOOL;
 }
 
 YYTK::RValue::RValue(
@@ -45,7 +55,7 @@ YYTK::RValue::RValue(
 }
 
 YYTK::RValue::RValue(
-	IN std::string_view Value, 
+	IN std::string_view Value,
 	IN YYTKInterface* Interface
 )
 {
@@ -55,10 +65,69 @@ YYTK::RValue::RValue(
 	// Let's not crash on invalid interfaces provided
 	if (!Interface)
 		return;
-	
+
 	// We can ignore this, because if it fails, we're just initialized to UNSET
 	Interface->StringToRValue(
 		Value,
 		*this
 	);
+}
+
+bool YYTK::RValue::AsBool()
+{
+	switch (this->m_Kind)
+	{
+	case VALUE_REAL:
+	case VALUE_BOOL:
+		return this->m_Real > 0.5;
+	case VALUE_PTR:
+	case VALUE_OBJECT:
+		return this->m_Pointer != nullptr;
+	case VALUE_UNDEFINED:
+		return false;
+	case VALUE_INT32:
+	case VALUE_REF:
+		return this->m_i32 > 0;
+	case VALUE_INT64:
+		return this->m_i64 > 0;
+	default:
+		// Bool argument has incorrect type!
+		assert(false);
+	}
+
+	return false;
+}
+
+double YYTK::RValue::AsReal()
+{
+	switch (this->m_Kind)
+	{
+	case VALUE_REAL:
+	case VALUE_BOOL:
+		return this->m_Real;
+	case VALUE_INT32:
+	case VALUE_REF:
+		return static_cast<double>(this->m_i32);
+	case VALUE_INT64:
+		return static_cast<double>(this->m_i64);
+	default:
+		// Real argument has incorrect type!
+		assert(false);
+	}
+
+	return 0.0;
+}
+
+std::string_view YYTK::RValue::AsString(
+	IN YYTKInterface* Interface
+)
+{
+	// Let's not crash on invalid interfaces provided
+	if (!Interface)
+		return "";
+
+	if (!Interface->GetRunnerInterface().YYGetString)
+		return "";
+
+	return Interface->GetRunnerInterface().YYGetString(this, 0);
 }
